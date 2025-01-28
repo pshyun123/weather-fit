a147965525e22ea226926dd95900c90995c92818
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2025-01-29 00:05:08 +0900

    app.js 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./util/GlobalStyle";
+import Layout from "./pages/Layout";
+import MainPage from "./pages/MainPage";
+import Login from "./pages/LoginPage";
+import Mypage from "./pages/Mypage";
+import Join from "./pages/Join";
+import WeatherDetailPage from "./pages/WetherDetailPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/weatherdetailpage" element={<WeatherDetailPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
